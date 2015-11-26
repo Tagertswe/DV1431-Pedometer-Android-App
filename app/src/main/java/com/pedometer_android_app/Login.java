01f0e82a77f2f8bddc385c9a13e226d7f9f34a05
Pol Haghverdian <pol_r@hotmail.com>
2015-11-26 16:06:22 +0100

    Added login.java and also the xml file

---
+++ app/src/main/java/com/pedometer_android_app/Login.java
@@ -0,0 +1,15 @@
+package com.pedometer_android_app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by pol on 11/26/15.
+ */
+public class Login extends Activity {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
