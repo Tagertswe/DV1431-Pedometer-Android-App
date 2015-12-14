0b14832ef508ac705d315a63c3dca63f3d6bdcde
Pol Haghverdian <pol_r@hotmail.com>
2015-12-14 11:57:23 +0100

    Pedometer view GUI done

--- app/src/main/java/com/pedometer_android_app/TabFragmentPedometer.java
+++ app/src/main/java/com/pedometer_android_app/TabFragmentPedometer.java
@@ -8,11 +8,32 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class TabFragmentPedometer extends Fragment {
 
+    private static View view;
+    private Button startButton;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.pedometer_view, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        view = inflater.inflate(R.layout.pedometer_view, container, false);
+
+        startButton = ((Button) view.findViewById(R.id.start_button));
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View r) {
+
+                System.out.println("start!!!");
+
+            }
+        });
+
+        return view;
     }
+
+
 }
